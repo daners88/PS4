712aac1552068b41256cdfd9d83be8c370431223
Dane <daners88@yahoo.com>
2019-09-17 23:11:40 -0600

    changed movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,61 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController _controller;
-    public float moveSpeed = 10.0f;
-    Transform playerPos;
-    private float rotateDegrees = 90.0f;
-    Vector3 move = Vector3.zero;
-    // Start is called before the first frame update
+    private Rigidbody rb;
+    public float speed = 1500.0f;
+    public float rotSpeed = 50.0f;
+    private float verticalMovement;
+    private float horizontalMovement;
     void Start()
     {
-        _controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Quaternion thing = transform.localRotation;
-        if(thing.eulerAngles.y == 0.0)
-        {
-            move = new Vector3(0, 0, Input.GetAxis("Vertical"));
-        }
-        else if(thing.eulerAngles.y == 90.0)
-        {
-            move = new Vector3(Input.GetAxis("Vertical"), 0, 0);
-        }
-        else if(thing.eulerAngles.y == 270.0)
-        {
-            move = new Vector3(-Input.GetAxis("Vertical"), 0, 0);
-        }
-        else if (thing.eulerAngles.y == 180.0)
-        {
-            move = new Vector3(0, 0, -Input.GetAxis("Vertical"));
-        }
-        move *= moveSpeed;
-        _controller.Move(move * Time.deltaTime);
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            Rotate(-rotateDegrees);
-        }
-        else if(Input.GetKeyUp(KeyCode.D))
-        {
-            Rotate(rotateDegrees);
-        }
-       
-        
+        //Quaternion thing = transform.localRotation;
+        //if(thing.eulerAngles.y == 0.0)
+        //{
+        //    move = new Vector3(0, 0, Input.GetAxis("Vertical"));
+        //}
+        //else if(thing.eulerAngles.y == 90.0)
+        //{
+        //    move = new Vector3(Input.GetAxis("Vertical"), 0, 0);
+        //}
+        //else if(thing.eulerAngles.y == 270.0)
+        //{
+        //    move = new Vector3(-Input.GetAxis("Vertical"), 0, 0);
+        //}
+        //else if (thing.eulerAngles.y == 180.0)
+        //{
+        //    move = new Vector3(0, 0, -Input.GetAxis("Vertical"));
+        //}
+        //move *= moveSpeed;
+        //_controller.Move(move * Time.deltaTime);
+        //if (Input.GetKeyUp(KeyCode.A))
+        //{
+        //    Rotate(-rotateDegrees);
+        //}
+        //else if(Input.GetKeyUp(KeyCode.D))
+        //{
+        //    Rotate(rotateDegrees);
+        //}
+        verticalMovement = Input.GetAxis("Vertical");
+        horizontalMovement = Input.GetAxis("Horizontal");
+        rb.velocity = (transform.forward * verticalMovement) * speed * Time.fixedDeltaTime;
+        transform.Rotate((transform.up * horizontalMovement) * rotSpeed * Time.fixedDeltaTime);
+
     }
 
-    void Rotate(float degrees)
-    {
-        var startRotation = transform.rotation;
-        var startPosition = transform.position;
-        transform.RotateAround(transform.position, Vector3.up, degrees);
-        var endRotation = transform.rotation;
-        var endPosition = transform.position;
-        transform.rotation = startRotation;
-        transform.position = startPosition;
-        transform.rotation = endRotation;
-        transform.position = endPosition;
-    }
+    //void Rotate(float degrees)
+    //{
+    //    var startRotation = transform.rotation;
+    //    var startPosition = transform.position;
+    //    transform.RotateAround(transform.position, Vector3.up, degrees);
+    //    var endRotation = transform.rotation;
+    //    var endPosition = transform.position;
+    //    transform.rotation = startRotation;
+    //    transform.position = startPosition;
+    //    transform.rotation = endRotation;
+    //    transform.position = endPosition;
+    //}
 }
